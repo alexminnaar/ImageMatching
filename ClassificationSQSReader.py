b007c07768c60f55b3370dfd4462e448526591f0
Alex Minnaar <alexminnaar@ip-10-0-1-240.ec2.internal>
2017-03-30 15:04:58 -0700

    removed polling timeouts

--- ClassificationSQSReader.py
+++ ClassificationSQSReader.py
@@ -1,8 +1,6 @@
 import boto3
 from ImageClassifier import CustomImageClassifier
-# from TitleClassifier import TitleClassifier
 import multiprocessing
-import json
 from pymemcache.client.base import Client
 import sys
 import logging
@@ -11,10 +9,6 @@ from time import sleep
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
-
-
-# PREFIX = "ImageMatcherService"
-# SEPARATOR = 0x1e
 
 
 def sqs_polling(queue_name, memcache_endpoint, min_prob, process_id):
@@ -36,22 +30,8 @@ def sqs_polling(queue_name, memcache_endpoint, min_prob, process_id):
 
     no_messages = False
 
-    refresh_counter = 0
-
     # poll sqs forever
     while 1:
-
-        refresh_counter += 1
-        logger.warning('Process %d: refresh counter value %d' % (process_id, refresh_counter))
-
-        # every 20th poll, reconnect to sqs to prevent stale connection
-        if refresh_counter % 20 == 0:
-            logger.warning('Process %d: refreshing connection' % process_id)
-            queue = sqs.get_queue_by_name(QueueName=queue_name)
-            refresh_counter = 0
-
-        # polling delay so aws does not throttle us
-        sleep(3.0)
 
         # sleep longer if there are no messages on the queue the last time it was polled
         if no_messages:
@@ -75,7 +55,7 @@ def sqs_polling(queue_name, memcache_endpoint, min_prob, process_id):
             # get image url from message
             image_url = message.body
 
-            # get image prediction
+            # predict image category and persist to memcache
             try:
                 image_pred = image_clf.run_inference_on_image(image_url)
 
@@ -105,7 +85,7 @@ def main():
     memcache_endpoint = sys.argv[2]
     min_prob = float(sys.argv[3])
 
-    # keep track of processes to restart if needed. PID => Process
+    # keep track of processes in dictionary to restart if needed i.e. {PID: Process}
     processes = {}
 
     num_processes = range(1, 9)
